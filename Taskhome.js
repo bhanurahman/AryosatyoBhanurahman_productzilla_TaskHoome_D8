8fe24a6f1d971b0630d0abc7c1fd986044024251
Aryosatyo Bhanurahman <bhanurahman919@gmail.com>
2023-03-09 01:12:53 +0700

    inisiasi project

---
+++ Taskhome.js
@@ -0,0 +1,13 @@
+let wadah = ['jeruk','jeruk ibu','jeruk ayah','jeruk siapa kek'];
+
+console.log('Array push');
+
+wadah.push('rokok','asbak')
+console.log(wadah);
+
+console.log('===','Nomer 2','===');
+console.log(wadah[2]);
+
+console.log('===','Nomer 3','===');
+wadah[2] = 'kaki'
+console.log(wadah);
